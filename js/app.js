6d336d587b63b2535407e993729ff1f63adeb5c7
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-01-05 14:23:35 +0100

    update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("chargé");
     let btn, warning, email, password;
     let exprmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    let exprmdp = /^[a-zA-Z0-9._-]{2,15}$/;
     warning = document.querySelector(".warning");
     email = document.getElementsByTagName("input")[0].value;
     password = document.getElementsByTagName("input")[1].value;
@@ -14,16 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let login = () => {
         email = document.getElementsByTagName("input")[0].value;
         password = document.getElementsByTagName("input")[1].value;
-        if (exprmail.test(email) && password == "beubeuh") {
+        if (exprmail.test(email) && exprmdp.test(password)) {
             localStorage.setItem("user", email);
             sessionStorage.setItem("user", "sessionid");
             document.location.assign("accueil.html");
-        } else if (!exprmail.test(email)) {
-            warning.classList.add("error");
+        } else if (!exprmail.test(email) || !exprmdp.test(password)) {
             warning.innerText = "Mail ou mot de passe invalide";
             warning.classList.remove("success");
         } else {
-            warning.classList.add("error");
             warning.innerText = "Mail ou mot de passe invalide";
             warning.classList.remove("success");
         };
